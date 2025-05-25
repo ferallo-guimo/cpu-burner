0e1f4424747780df4cd6040faa0a794507905a75
Hüseyin Berat Balkan <ferallo.guimo@gmail.com>
2025-05-25 19:44:26 +0300

    MultiCore usage to actually use all cores added. (CTRL+C doesn`t work yet)

--- burn.py
+++ burn.py
@@ -1,4 +1,19 @@
-n = 1
-while True:
-    n *= 2
-    print(n)
+import multiprocessing
+
+def burn_cpu():
+    x = 0
+    while True:
+        x += 1  # endless loop doing math
+
+if __name__ == "__main__":
+    num_cores = multiprocessing.cpu_count()
+    print(f"Starting {num_cores} processes to max out CPU")
+
+    processes = []
+    for _ in range(num_cores):
+        p = multiprocessing.Process(target=burn_cpu)
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
